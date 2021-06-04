5616fd08624f05f3a99585266485116ebe8bed71
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-04 23:13:45 +0300

    make the changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
-from .config import DevConfig
-from flask_bootstrap import  Bootstrap
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+from flask_bootstrap import Bootstrap
+from config import config_options
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+bootstrap = Bootstrap()
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+def create_app(config_name):
 
-from app import views
-from app import error
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app
