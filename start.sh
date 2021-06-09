792ec8bce0f3b73a089aa375c71f60c9abd4f908
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-09 15:01:55 +0300

    update changes

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
- export MOVIE_API_KEY=14900549836564fe93311acf17aafa1f
-export SECRET_KEY=88881994
+ export MOVIE_API_KEY='14900549836564fe93311acf17aafa1f'
+export SECRET_KEY='88881994'
 
 python3  manage.py server
