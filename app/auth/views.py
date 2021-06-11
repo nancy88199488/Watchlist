71c9d6c7b9388c8c11d84325d729b22188d8eef9
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-11 19:37:06 +0300

    update the files

--- app/auth/views.py
+++ app/auth/views.py
@@ -34,4 +34,5 @@ def login():
 @login_required
 def logout():
     logout_user()
+    flash('You have been successfully logged out')
     return redirect(url_for("main.index"))
--- app/models.py
+++ app/models.py
@@ -59,6 +59,8 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(255),index=True)
     email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     password_hash = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
 
