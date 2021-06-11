e87c44104f1bb35dfb8f10d68bda66730b1c6a3c
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-11 17:26:19 +0300

    update the files

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from .import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 class Movie:
     '''
     Movie class to define Movie Objects
@@ -46,12 +48,13 @@ class Review:
         return response
 
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255),index=True)
+    email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-
+    password_hash = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
 
     @property
