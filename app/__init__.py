9562eacb706023648ce3b85a134223bb5c953d9f
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-06-25 18:04:14 +0300

    update all files

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
@@ -7,16 +8,17 @@ from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 from flask_simplemde import SimpleMDE
 
-mail = Mail
+
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 simple = SimpleMDE()
-photos = UploadSet('photos',IMAGES)
 
+
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
 
 def create_app(config_name):
 
@@ -31,20 +33,21 @@ def create_app(config_name):
     login_manager.init_app(app)
     mail.init_app(app)
     simple.init_app(app)
+    
 
 
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
-
 
     # setting config
     from .requests import configure_request
     configure_request(app)
 
-     # configure UploadSet
+    #configure UploadSet
     configure_uploads(app,photos)
 
     return app
